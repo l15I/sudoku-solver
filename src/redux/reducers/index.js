5e21d59b983dcc5aa1738b26e100e183aa1c5300
Meteo <>
2019-08-27 22:13:11 +0200

    Removed routing reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,4 @@
 import { combineReducers } from 'redux'
-import page from './page'
 import grid from './grid'
 
-export default combineReducers({ page, grid })
+export default combineReducers({ grid })
