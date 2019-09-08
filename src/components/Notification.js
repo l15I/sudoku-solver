1e7fa590d484273017072b065cc5e868ce66ec9f
Meteo <>
2019-09-08 17:23:41 +0200

    Fixed bug: message disappeared before animation ended

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,21 +1,35 @@
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core'
-import { useEffect } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
 export default function (props) {
-  const error = useSelector(s => s.sudoku.error)
   const dispatch = useDispatch()
+  const error = useSelector(s => s.sudoku.error)
+
+  const prevError = useRef()
+  useEffect(() => {
+    prevError.current = error
+  })
+
+  const [top, setTop] = useState(-100)
   const style = css`
     color: blue;
     position: absolute;
-    top: ${error ? 10 : -100}px;
+    top: ${top}px;
     right: 10px;
     transition: top .5s linear;
   `
+
   useEffect(() => {
-    const id = setTimeout(() => dispatch({ type: 'CLEAN_ERROR_INFO' }), 3000)
-    return () => clearTimeout(id)
+    if (error) {
+      setTop(10)
+      const id = setTimeout(() => {
+        setTop(-100)
+        dispatch({ type: 'CLEAN_ERROR_INFO' })
+      }, 3000)
+      return () => clearTimeout(id)
+    }
   }, [dispatch, error])
-  return <div css={style}>{error}</div >
+  return <div css={style}>{error || prevError.current}</div >
 }
