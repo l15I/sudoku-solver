96d5375505a4f68d706e1e25f3223b05d0a4b65d
Meteo <>
2019-09-09 15:47:49 +0200

    Styles for navigation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Sudoku from './pages/Sudoku'
 import About from './pages/About'
+import Navigation from './components/Navigation'
 import Notification from './components/Notification'
 
 export default function App() {
   return <Router>
+    <Navigation />
     <Notification />
-    <nav>
-      <Link to='/'>about</Link>
-      <Link to='/sudoku'>sudoku</Link>
-    </nav>
     <Route path='/' exact component={About} />
     <Route path='/sudoku' component={Sudoku} />
   </Router >
---
+++ src/components/Navigation.js
@@ -0,0 +1,24 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
+import { Link } from 'react-router-dom'
+
+
+
+export default function () {
+  return <nav css={css`
+    display: flex;
+    margin: auto;
+    a {
+      color: black;
+      padding: 1em;
+      text-decoration: none;
+
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  `}>
+    <Link to='/'>About</Link>
+    <Link to='/sudoku'>Sudoku</Link>
+  </nav>
+}
